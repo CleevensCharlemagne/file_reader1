fb4afbacb177df9564ec817b655292ed0688eb20
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-03-14 22:41:41 -0400

    update

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
+// C program to implement
+// the above approach
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+// Driver code
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+
+	return 0;
 }
